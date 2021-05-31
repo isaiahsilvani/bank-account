551bc9a7953a1e771a8c4eaf3fa1b7f08475de79
isaiahsilvani <isaiahsilvani@outlook.com>
2021-05-31 14:03:23 -0400

    combine reducers

--- src/state/reducers/bankReducer.ts
+++ src/state/reducers/bankReducer.ts
@@ -1,16 +1,16 @@
+import { ActionType } from '../action-types/index'
 import { Action } from '../actions/index'
 
 const initialState = 0
 // Give the state and action arguments types
 
-
 const reducer = (state: number = initialState, action: Action) => {
   switch (action.type){
-    case "deposit":
+    case ActionType.DEPOSIT:
       return state + action.payload  //every action needs it's own type
-    case "withdraw":
+    case ActionType.WITHDRAW:
       return state - action.payload  //every action needs it's own type
-    case "bankrupt":  // try putting action.payload here and see the error. That's so cool!
+    case ActionType.BANKRUPT:  // try putting action.payload here and see the error. That's so cool!
       return 0
     default:
       return state
